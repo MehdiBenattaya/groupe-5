f55d2e05e1ec461303160f1ce147a73799a78be0
Mehdi Benattaya <mehdi.benattaya@esprit.tn>
2022-11-06 22:11:55 +0100

    Create interfacenews.cpp

---
+++ Gestion_dactualte/interfacenews.cpp
@@ -0,0 +1,414 @@
+#include "interfacenews.h"
+#include "ui_interfacenews.h"
+#include "actualite.h"
+#include <QString>
+#include <QMessageBox>
+#include <QDate>
+#include <QIntValidator>
+#include <QDateEdit>
+#include <QFileDialog>
+#include <QByteArray>
+#include <QImage>
+#include <QBuffer>
+
+
+interfacenews::interfacenews(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::interfacenews)
+{
+    ui->setupUi(this);
+
+//ui->le_id->setValidator (new QIntValidator(100, 999, this));
+//listView
+
+ui->listView->setModel(A.afficher());
+ui->le_imgurl->setVisible(false);
+ui->bouton_supp->setVisible(false);
+ui->frame_8->setVisible(false);
+
+
+}
+
+interfacenews::~interfacenews()
+{
+    delete ui;
+}
+
+
+
+
+// ----------------------------ajout-------------------------------------------
+
+void interfacenews::on_pushButton_clicked()
+{
+
+
+  /*  QDate date = QDate::currentDate();
+       QString dateString = date.toString();
+       ui->le_date->setDate(dateString);
+    QDate date = QDate::currentDate();
+        ui->DateEdit->setDate(date);
+   QDate date = QDate::currentDate();
+      ui->le_date->setDate(date);
+    QDate date= ui->le_datea->text();*/ //date edit false
+
+
+    QString id= ui->le_id->text();
+    QString Titre= ui->le_titre->text();
+    QString lieu= ui->le_lieu->text();
+
+    QDate date= ui->le_date->date(); // this is only vlaid if u visually design the window and placed dateEdit
+    QString dateString = date.toString();
+
+    QString source= ui->le_source->text();
+
+    QString categorie= ui->le_categorie->currentText();
+
+
+QString description= ui->le_description->toPlainText();
+
+
+QString imageurl=ui->le_imgurl->text();
+
+
+
+//********************************
+
+
+
+
+    Actualite A( id, Titre , lieu, date, source,categorie,description,imageurl);
+
+    bool test=A.ajouter();
+    if (test)
+    { QMessageBox::information(nullptr, QObject::tr("OK"), QObject::tr("ajout effectué.\n" "Click Cancel to exit."), QMessageBox::Cancel);
+    }
+    else
+        QMessageBox::critical(nullptr, QObject::tr("not OK"), QObject::tr("ajout non effectué.\n" "Click Cancel to exit."), QMessageBox::Cancel);
+
+}
+
+// -----------------------------------------------------------------------
+
+
+
+
+// ------------------------afficher rafraichir-----------------------------------------------
+
+void interfacenews::on_rafraichir_clicked()
+{
+  Actualite A;
+  ui->listView->setModel(A.afficher());
+}
+
+
+// ---------------------------------supprimer--------------------------------------
+
+void interfacenews::on_bouton_supp_clicked()
+{
+Actualite A1;
+A1.setid(ui->le_suppr->text());
+bool test=A1.supprimer(A1.getid());
+
+QMessageBox msgBox;
+if (test)
+    msgBox.setText("Suppression avec succes.");
+   else
+    msgBox.setText("Echec de suppression.") ;
+msgBox.exec();
+
+
+
+
+ui->bouton_supp->setVisible(false);
+
+ui->frame_8->setVisible(false);
+
+
+Actualite A;
+ui->listView->setModel(A.afficher());
+
+
+}
+
+
+// ---------------------------------list--------------------------------------
+
+
+
+
+void interfacenews::on_listView_activated(const QModelIndex &index)
+{
+
+
+   QString id=ui->listView->model()->data(index).toString();
+
+
+
+
+QSqlQuery qry;
+qry.prepare("select * FROM ACTUALITE WHERE IDP='"+id+"' or TITRE='"+id+"' or LIEU='"+id+"' or DATEA='"+id+"' or SOURCE='"+id+"' or DESCRIPTION='"+id+"' " );
+
+
+if (qry.exec())
+    {   while (qry.next())
+            {
+         ui->le_suppr->setText(qry.value(0).toString());
+
+
+
+              ui->le_id_2->setText(qry.value(0).toString());
+               ui->le_titre_2->setText(qry.value(1).toString());
+                ui->le_lieu_2->setText(qry.value(2).toString());
+
+               /* QString date_string = qry.value(3).toString();
+                 QDate Date = QDate::fromString(date_string);
+                  ui->le_date_2->setDisplayFormat(date_string);*/
+
+                  ui->le_source_2->setText(qry.value(4).toString());
+
+
+              //   QSqlQueryModel *modal=new QSqlQueryModel();
+               //  modal    ->setQuery (qry);
+         //    ui->le_categorie_2->findText(qry.value(4).toString());
+
+
+
+             ui->le_description_2->setPlainText(qry.value(6).toString());
+
+
+                  QImage image;
+                 image.load (qry.value(7).toString());
+
+
+                      image= image.scaledToWidth(ui->le_imageapercu->width(),Qt::SmoothTransformation);
+                      ui->le_imageapercu->setPixmap(QPixmap::fromImage(image));
+
+
+                      image= image.scaledToWidth(ui->le_image_2->width(),Qt::SmoothTransformation);
+                      ui->le_image_2->setPixmap(QPixmap::fromImage(image));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+    }
+
+}
+
+
+// -----------------------------------------------------------------------
+
+
+// ---------------------------modifier--------------------------------------------
+
+
+
+void interfacenews::on_bouton_modif_2_clicked()
+{
+
+
+
+    Actualite A1;
+    A1.setid(ui->le_suppr->text());
+
+
+
+
+
+    QString id= ui->le_id_2->text();
+    QString Titre= ui->le_titre_2->text();
+    QString lieu= ui->le_lieu_2->text();
+
+    QDate date= ui->le_date_2->date(); // this is only vlaid if u visually design the window and placed dateEdit
+    QString dateString = date.toString();
+
+    QString source= ui->le_source_2->text();
+
+    QString categorie= ui->le_categorie_2->currentText();
+
+    QString description= ui->le_description_2->toPlainText();
+
+
+QString imageurl;
+
+    Actualite A( id, Titre , lieu, date, source,categorie,description,imageurl);
+
+    bool test=A.modifier();
+    if (test)
+    { QMessageBox::information(nullptr, QObject::tr("OK"), QObject::tr("modification effectué.\n" "Click Cancel to exit."), QMessageBox::Cancel);
+    }
+    else
+        QMessageBox::critical(nullptr, QObject::tr("not OK"), QObject::tr("modification non effectué.\n" "Click Cancel to exit."), QMessageBox::Cancel);
+
+
+    ui->bouton_supp->setVisible(false);
+    ui->frame_8->setVisible(false);
+
+
+
+
+    ui->listView->setModel(A.afficher());
+
+
+}
+
+void interfacenews::on_saisir_clicked()
+{
+    ui->bouton_supp->setVisible(true);
+
+    ui->frame_8->setVisible(true);
+
+}
+
+
+// ---------------------------ajout d'image--------------------------------------------
+
+void interfacenews::on_pushButton_10_clicked()
+{
+
+
+        QString filename = QFileDialog::getOpenFileName(this, tr("choose"),"",tr("Image (*.png *.jpg *.jpeg *.bmp *.gif)"));
+
+        if (QString::compare(filename, QString())!=0)
+        {
+            QImage image;
+            bool valid= image.load (filename);
+
+            if (valid)
+            {
+                image= image.scaledToWidth(ui->le_image->width(),Qt::SmoothTransformation);
+                ui->le_image->setPixmap(QPixmap::fromImage(image));
+
+               ui->le_imgurl->setText(filename);
+
+            }
+
+
+            else
+            {
+                //error handling
+            }
+
+        }
+
+    }
+
+
+
+//--------------------------------modif d'image-----------------------------------------------
+
+
+
+
+void interfacenews::on_pushButton_11_clicked()
+{
+    QString filename = QFileDialog::getOpenFileName(this, tr("choose"),"",tr("Image (*.png *.jpg *.jpeg *.bmp *.gif)"));
+
+    if (QString::compare(filename, QString())!=0)
+    {
+        QImage image;
+        bool valid= image.load (filename);
+
+        if (valid)
+        {
+            image= image.scaledToWidth(ui->le_image->width(),Qt::SmoothTransformation);
+            ui->le_image->setPixmap(QPixmap::fromImage(image));
+
+           ui->le_imgurl->setText(filename);
+
+        }
+
+
+        else
+        {
+            //error handling
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//-----------------------------RECHERCHER----------------------------------------
+
+void interfacenews::on_pushButton_7_clicked()
+{
+
+    Actualite A1;
+    QString id ;
+         id= ui->le_recherche->text();
+    ui->listView->setModel(A1.rechercher(id));
+
+
+
+}
+
+
+//-----------------------------rechercherparcategorie----------------------------------------
+
+void interfacenews::on_categoriechercher_activated()
+{
+    Actualite A2;
+
+        QString categorie= ui->categoriechercher->currentText();
+
+          ui->listView->setModel(A2.recherchercategorie(categorie));
+
+
+
+
+
+
+}
+
+
+//-----------------------------trier----------------------------------------
+
+
+void interfacenews::on_trier_clicked()
+{
+    Actualite A2;
+
+    ui->listView->setModel(A2.trier());
+
+
+}
