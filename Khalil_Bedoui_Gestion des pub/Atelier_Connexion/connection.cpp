039ef7e6e6992ce650f64b20971e08f31752ff40
Bedoui2002 <bedoui.khalil@esprit.tn>
2022-11-07 11:56:41 +0100

    Create connection.cpp

---
+++ Khalil_Bedoui_Gestion des pub/Atelier_Connexion/connection.cpp
@@ -0,0 +1,20 @@
+#include "connection.h"
+
+Connection::Connection()
+{
+}
+
+bool Connection::createconnection()
+{bool test=false;
+QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
+db.setDatabaseName("TEST_BD");
+db.setUserName("system");//inserer nom de l'utilisateur
+db.setPassword("system");//inserer mot de passe de cet utilisateur
+
+if (db.open())
+test=true;
+
+    return  test;
+}
+void Connection::closeConnection()
+{db.close();}
